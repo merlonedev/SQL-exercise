5f1a44f26594c9f5f420d490a9f3a917da6fac46
alestori7@gmail.com <alestori7@gmail.com>
2021-09-20 11:49:21 -0300

    exercise 1.8

--- SQL_exercise_01/solutions.sql
+++ SQL_exercise_01/solutions.sql
@@ -1,7 +1,68 @@
+use sqlexercises;
+
 -- exercise 1
 
 SELECT 
     `Name`
 FROM
-    sqlexercises.Products;
+    Products;
     
+-- exercise 1.2
+
+SELECT 
+    p.name, p.price
+FROM
+    Products p;
+    
+-- exercise 1.3
+
+SELECT 
+    p.name
+FROM
+    Products p
+WHERE
+    p.Price <= 200;
+    
+-- exercise 1.4
+
+SELECT 
+    p.name
+FROM
+    Products p
+WHERE
+    p.Price BETWEEN 60 AND 120;
+    
+-- exercise 1.5
+
+SELECT 
+    p.price * 100 AS PrinceInCents
+FROM
+    Products p;
+    
+-- exercise 1.6
+
+SELECT 
+    AVG(price) AS Average
+FROM
+    Products;
+
+-- exercise 1.7
+
+SELECT 
+    AVG(price)
+FROM
+    Products
+WHERE
+    Manufacturer = 2;
+    
+-- exercise 1.8
+
+SELECT 
+    COUNT(*)
+FROM
+    Products
+WHERE
+    Price >= 180;
+    
+-- exercise 1.9
+
