0d261b3d4aa4e08c9bcbba0282bc8a6a90ebd0fe
alestori7@gmail.com <alestori7@gmail.com>
2021-09-20 13:19:33 -0300

    exercise 2.2 to 2.5

--- SQL_exercise_02/solutions.sql
+++ SQL_exercise_02/solutions.sql
@@ -5,3 +5,40 @@ SELECT
     LastName
 FROM
     Employees;
+    
+-- exercise 2.2
+
+SELECT DISTINCT
+    LastName
+FROM
+    Employees;
+    
+-- exercise 2.3
+
+SELECT 
+    *
+FROM
+    Employees
+WHERE
+    LastName = 'Smith';
+    
+-- exercise 2.4
+
+SELECT 
+    *
+FROM
+    Employees
+WHERE
+    LastName IN ('Smith' , 'Doe');
+    
+-- exercise 2.5
+
+SELECT 
+    *
+FROM
+    Employees
+WHERE
+    Department = 14;
+    
+-- exercise 2.6
+
